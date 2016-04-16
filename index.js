f70f236500e1725d5a73d9fcefcb3ff5905a0d95
suyu34 <joooye1991@gmail.com>
2016-04-17 03:27:38 +0800

    [UPDATE] et-template version

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 'use strict'
 
 let loaderUtils = require('loader-utils')
-let ET = require('et-template')
+let et = require('et-template')
 
 module.exports = function (content) {
   if (this.cacheable) this.cacheable()
 
   let options = loaderUtils.parseQuery(this.query)
-  let et = new ET(options)
   return et.compile(content, options)
 }
